04953d5d0e8b80e935826cee6d5d482a1b14209e
mike spaans <99069233@mydavinci.nl>
2023-04-19 13:13:54 +0200

    pokemon assignment 1 afgemaakt

---
+++ Pokemon_Simulator/Pokemon.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Pokemon;
+
+    public class Charmander
+    {
+        public string Name { get; set; }
+        public string Strength { get; set; }
+        public string Weakness { get; set; }
+
+        
+    public Charmander(string Name, string Strength, string Weakness)
+    {
+        this.Name = Name;
+        this.Strength = Strength;
+        this.Weakness = Weakness;
+    }
+
+
+    }
+
+
+
+
---
+++ Pokemon_Simulator/Program.cs
@@ -0,0 +1,39 @@
+﻿using Pokemon;
+using System;
+
+
+Console.WriteLine("what is the name of the charmander?");
+string CharmanderName = Console.ReadLine();
+var charmander = new Charmander(CharmanderName, "Fire", "Water");
+bool isPlaying = true;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{charmander.Name}!!!!");
+}
+
+//3. The charmander does its battle cry for ten times.
+
+while (isPlaying == true)
+{
+
+
+
+    Console.WriteLine("what is the new name of the charmander?");
+    string CharmanderName2 = Console.ReadLine();
+    charmander.Name = CharmanderName2;
+
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"{charmander.Name}!!!!");
+    }
+
+    Console.WriteLine("Do you want to continue? (y/n)");
+    string answer = Console.ReadLine();
+    if (answer == "n")
+    {
+        isPlaying = false;
+    }
+
+
+}
