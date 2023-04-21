0917223da3fe12bbec224c340d7722936354f156
mike spaans <99069233@mydavinci.nl>
2023-04-21 14:31:14 +0200

    alle functies en classes gemaakt

--- Pokemon_Simulator/Pokemon.cs
+++ Pokemon_Simulator/Pokemon.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.Collections.Generic;
+using System;
+
 namespace Pokemon;
 
     public class Charmander
@@ -7,16 +9,124 @@ namespace Pokemon;
         public string Strength { get; set; }
         public string Weakness { get; set; }
 
+        public int CharmanderNumber { get; set; }
+
+        public Boolean IsReleased { get; set; }
+
         
-    public Charmander(string Name, string Strength, string Weakness)
+    public Charmander(string Name, string Strength, string Weakness, int CharmanderNumber, bool IsReleased)
     {
         this.Name = Name;
         this.Strength = Strength;
         this.Weakness = Weakness;
+        this.CharmanderNumber = CharmanderNumber;
+        this.IsReleased = IsReleased;
     }
 
 
     }
+
+
+/*
+The pokeball is empty or it can contain a single charmander.
+The pokeball can be thrown, which opens it up, and then releases the charmander inside of it.
+The charmander can be returned back to its pokeball, which closes the pokeball again.
+ */
+    public class Pokeball
+        {
+
+    public static List<Pokeball> PokeballsBelt = new List<Pokeball>();
+    public static List<Pokeball> PokeballsBelt2 = new List<Pokeball>();
+
+
+
+    public Boolean IsFull { get; set; }
+    public int PokeballNumber { get; set; }
+
+    public Charmander charmander { get; set; }
+
+        public Pokeball (Boolean IsFull, int PokeballNumber, Charmander charmander, List<Pokeball> PokeBallsList)
+            {
+            this.IsFull = IsFull;
+            this.PokeballNumber = PokeballNumber;
+            this.charmander = charmander;
+            
+
+            PokeBallsList.Add(this);
+            Console.WriteLine(PokeBallsList.Count);
+                    
+            }
+            
+
+            public static void Throw(int PokeballNumber, List<Pokeball> PokeballList)
+            {
+                var PokeBall = PokeballList[PokeballNumber];
+                //charmander gets released
+                PokeBall.IsFull = false;
+                PokeBall.charmander.IsReleased = true;
+                
+               // return PokeballNumber;
+            }
+
+            public static void Return(int PokeballNumber, List<Pokeball> PokeballList)
+            {
+                //charmander gets returned
+                var PokeBall = PokeballList[PokeballNumber];
+                PokeBall.IsFull = true;
+                PokeBall.charmander.IsReleased = false;
+            }
+            
+            
+
+        }
+
+
+
+
+/*
+The trainer has a name and a belt with six pokeballs.
+The trainer can throw a pokeball from their belt.
+The trainer can return a pokemon back to its pokeball and put the pokeball back on their belt.
+There are restrictions to the Trainer class:
+The belt has six pokeballs with a Charmander in each of them.
+The belt cannot be an array but has to be a List<Pokeball> class.
+ */
+public class Trainer
+{
+    //private List<Pokeball> PokeballsBelt = new List<Pokeball>();
+
+    public string Name { get; set; }
+
+    public Trainer(string Name, List<Pokeball> PokeballList)
+    {
+        this.Name = Name;
+        FillBelt(PokeballList);
+    }
+
+    public static void FillBelt(List<Pokeball> PokeballList)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            var charmander = new Charmander("Charmander", "Fire", "Water", i, false);
+            var Pokeball = new Pokeball(true, i, charmander, PokeballList);
+        }
+    }
+
+    public void Throw(int PokeballNumber, List<Pokeball> PokeballList)
+    {
+        Pokeball.Throw(PokeballNumber, PokeballList);
+    }
+
+    public void Return(int PokeballNumber, List<Pokeball> PokeballList)
+    {
+        Pokeball.Return(PokeballNumber, PokeballList);
+    }
+
+
+
+}
+
+
 
 
 
--- Pokemon_Simulator/Program.cs
+++ Pokemon_Simulator/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Pokemon;
 using System;
 
+//assignement 1
 
+/*
 Console.WriteLine("what is the name of the charmander?");
 string CharmanderName = Console.ReadLine();
 var charmander = new Charmander(CharmanderName, "Fire", "Water");
@@ -36,4 +38,29 @@ while (isPlaying == true)
     }
 
 
+}
+*/
+
+
+
+//assignement 2
+Console.WriteLine("what is the name of the first trainer?");
+string TrainerName = Console.ReadLine();
+var trainer = new Trainer(TrainerName, Pokeball.PokeballsBelt);
+
+Console.WriteLine("what is the name of the second trainer?");
+string TrainerName2 = Console.ReadLine();
+var trainer2 = new Trainer(TrainerName2, Pokeball.PokeballsBelt2);
+
+for (int i = 0; i < 6; i++)
+{
+    //Console.WriteLine($"trainer {trainer.Name} throws pokeball {i}");
+    var Test = Pokeball.PokeballsBelt[i];
+    Console.WriteLine($"{Test.charmander.Name}");
+    //trainer.Throw(i, Pokeball.PokeballsBelt);
+
+
+
+    //Console.WriteLine($"trainer {trainer2.Name} throws pokeball {i}");
+    //trainer2.Throw(i);
 }
