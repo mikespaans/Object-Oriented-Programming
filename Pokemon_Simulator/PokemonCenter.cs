c03fae4089a7f2e927c79f75917c59eb0fa919fd
mike spaans <99069233@mydavinci.nl>
2024-01-15 11:24:13 +0100

    fixed build error

--- Pokemon_Simulator/PokemonCenter.cs
+++ Pokemon_Simulator/PokemonCenter.cs
@@ -8,37 +8,38 @@ namespace Pokemon
         private Trainer trainer { get; set; }
         private Nurse Nurse { get; set; }
         private HealingStation HealingStation { get; set; }
-        
+
         public PokemonCenter(Trainer trainer)
         {
             this.trainer = trainer;
             this.Nurse = new Nurse();
             this.HealingStation = new HealingStation();
         }
-        
+
         public void HealPokemon()
         {
             int DeadPokemons = Nurse.CheckAllHealed(trainer.PokeballsBelt);
-            
+
             Console.WriteLine($"Number of dead pokemons: {DeadPokemons}");
-              
+
             // loop till all pokemons are healed                        
             while (DeadPokemons > 0)
             {
                 // fill the station slots
                 Nurse.FillStationSlots(HealingStation.Slots, trainer.PokeballsBelt);
-                
+
                 // heal the pokemons
                 HealingStation.CheckNurseAndHeal(Nurse);
-                
+
                 // return the healed pokemons to the belt
                 Nurse.ReturnPokeballsToBelt(HealingStation.Slots, trainer.PokeballsBelt);
-                
+
                 //check if all pokemons are healed
                 DeadPokemons = Nurse.CheckAllHealed(trainer.PokeballsBelt);
             }
-            
-            
+
+
         }
     }
+}
 
